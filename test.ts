c8f7ed3f7e4122a76aa8083e9840be333b2f9df6
john <42573088+zhangzhiyong0410@users.noreply.github.com>
2024-05-06 15:17:03 +0800

    update

--- test.ts
+++ test.ts
@@ -1,3 +1,10 @@
+/**
+ * Preface
+ * please test the separate content of each serial number title separately.
+ * don't download them all at once
+ */
+
+//1.Most visual modes
 input.onButtonPressed(Button.A, function () {
     basic.showString("" + (koi2.faceTrackingGetPosition(koi2.GetResult.ResultX)))
 })
@@ -5,7 +12,59 @@ input.onButtonPressed(Button.B, function () {
     basic.showString("" + (koi2.faceTrackingGetPosition(koi2.GetResult.ResultY)))
 })
 koi2.koi2Init(SerialPin.P2, SerialPin.P12)
+//  most visual modes are universal, they all have xy coordinates
+//  the following are all common. You can view the coordinates directly after switching modes.
+//      koi2.FullFunction.TrafficSign
+//      koi2.FullFunction.ObjectTracking
+//      koi2.FullFunction.FaceTracking
+//      koi2.FullFunction.FaceMask
+//      koi2.FullFunction.CustomModel
+//      koi2.FullFunction.NumberRecognition
+//      koi2.FullFunction.LetterRecognition
+//      koi2.FullFunction.ScanCod
 koi2.switchFunction(koi2.FullFunction.FaceTracking, koi2.IOTSwitch.OFF)
 basic.forever(function () {
     koi2.koi2UpdateData()
 })
+
+//2.ClassifyImage modes
+input.onButtonPressed(Button.A, function () {
+    koi2.classifyImageAddTagID("A")
+})
+input.onButtonPressed(Button.B, function () {
+    koi2.classifyImageAddTagID("B")
+})
+koi2.koi2Init(SerialPin.P2, SerialPin.P12)
+koi2.switchFunction(koi2.FullFunction.ClassifyImage, koi2.IOTSwitch.OFF)
+basic.forever(function () {
+    basic.showString(koi2.classifyImageGetClass())
+})
+basic.forever(function () {
+    koi2.koi2UpdateData()
+})
+
+//3.iot model
+input.onButtonPressed(Button.A, function () {
+    koi2.koi2MqttPub("/zzytest", "hello")
+})
+koi2.koi2MqttOnread(function (data, topic) {
+    basic.showString(data)
+})
+koi2.koi2Init(SerialPin.P2, SerialPin.P12)
+koi2.switchFunction(koi2.FullFunction.NoneMode, koi2.IOTSwitch.ON)
+koi2.koi2JoinAp("zy", "12345678")
+koi2.koi2MqttHost(
+    "iot.kittenbot.cn",
+    "clientid",
+    1883,
+    "",
+    ""
+)
+koi2.koi2MqttSub("/zzytest")
+basic.forever(function () {
+    koi2.koi2MqttRead()
+})
+basic.forever(function () {
+    koi2.koi2UpdateData()
+})
+
